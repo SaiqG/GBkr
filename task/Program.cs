fc5da4e91a78f1b6a070fc09b8d5856c738d57f4
SaiqG <akkorsakov@gmail.com>
2022-10-12 14:43:54 +0300

    Add loop

--- task/Program.cs
+++ task/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Linq;
+using System.Globalization;
 Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
 string[] EnterAndSplitString()
@@ -13,20 +14,29 @@ string[] SortStrings(string[] initString)
     int i = 0;
     foreach (string item in initString)
     {
-        if (item.Length <= 3)
+        if (item.Length < 4)
         {
             lessThen3digit[i++] = item;
         }
 
     }
     string[] result = new string[i];
+
     for (int j = 0; j < result.Length; j++)
     {
-       result[j] = lessThen3digit[j];
+        result[j] = lessThen3digit[j];
     }
 
     return result;
 }
 
-Console.Write("Заполните массив строк.");
-Console.WriteLine($"Только эти элементы вашего массива не длиннее трех символов : {string.Join(", ", SortStrings(EnterAndSplitString()))}");
+while (true)
+{
+    Console.Write("Заполните массив строк.");
+    Console.WriteLine($"Только эти элементы вашего массива не длиннее трех символов : {string.Join(", ", SortStrings(EnterAndSplitString()))}");
+
+    Console.WriteLine("Enter чтобы продолжить / q чтобы выйти");
+    ConsoleKeyInfo quite = Console.ReadKey();
+    Console.Clear();
+    if (quite.KeyChar == 'q' || quite.KeyChar == 'й') break;
+}
